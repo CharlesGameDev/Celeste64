936492863bd7539247c2522c3776ae06d8b94a1a
CharlesDev <charlesgamedev@gmail.com>
2025-07-10 08:29:27 -0400

    init online

---
+++ Celeste64.Server/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Celeste64.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Celeste64.Shared/Packets.cs
@@ -0,0 +1,11 @@
+﻿namespace Celeste64.Shared
+{
+    public enum Packets
+    {
+        PACKET_PLAYER_JOIN,
+        PACKET_PLAYER_LEAVE,
+        PACKET_PLAYER_UPDATE,
+
+        PACKET_ACTOR_UPDATE
+    }
+}
